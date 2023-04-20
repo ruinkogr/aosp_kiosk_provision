7b0287c7107f206d59472e2a184b61b1f7e20ce4
Konstantin Grenaderov <ruinkogr@gmail.com>
2023-04-20 19:24:00 +0300

    Provision Kiosk app as Device Owner

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -43,9 +43,10 @@
 #$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/core_intermediates)
 #$(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/Provision)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/product/priv-app/Provision)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/priv-app/Provision)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/KioskProvision)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/product/priv-app/KioskProvision)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/priv-app/KioskProvision)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system_ext/priv-app/KioskProvision)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
